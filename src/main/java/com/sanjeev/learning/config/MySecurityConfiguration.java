2e2f311d0ca0b8dddb08a1aa06ebf57b4b66c228
Sanjeev Kumar <sanjeevf800@gmail.com>
2024-01-10 16:18:00 +0530

    added comments to the commits

--- src/main/java/com/sanjeev/learning/config/MySecurityConfiguration.java
+++ src/main/java/com/sanjeev/learning/config/MySecurityConfiguration.java
@@ -28,6 +28,7 @@ public class MySecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
+		//Done
 		
 		http
 		.csrf()
